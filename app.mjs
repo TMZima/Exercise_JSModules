89ebfdd1363938a1918763fe8193fa367f1628ce
Trey Zima <trey.m.zima@gmail.com>
2024-11-24 17:13:32 -0700

    Completed Exercise 5

--- app.mjs
+++ app.mjs
@@ -1,5 +1,10 @@
+// Exercise 3 - Re-exporting Modules
 import { capitalize, square } from "./utils/index.mjs";
 
 console.log(capitalize("imported and "));
 console.log(capitalize("exported"));
 console.log(square(7));
+
+// Exercise 5 - Code is executed automatically upon import
+// Even though the above code is imported and called, the following code will still run before it because it is not inside a function or class.
+import "./globalConfig.mjs";
